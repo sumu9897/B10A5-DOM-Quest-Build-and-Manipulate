8fbef930bad0df8d1c117d239cb1322c13aa3796
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-09-23 20:35:29 +0600

    Finished All

--- js/index.js
+++ js/index.js
@@ -62,13 +62,13 @@ const donateAllEl = document.getElementById("donate-fields");
 const historyAllEl = document.getElementById("history-fields");
 
 donateBtn.addEventListener("click", function () {
-  donateBtn.classList.add("bg-primary", "p-[12px_32px_12px_32px]", "text-black");
+  donateBtn.classList.add("bg-primary",  "text-black");
 
-  donateBtn.classList.remove("border-2", "border-solid",  "p-[10px_30px_10px_30px]", "text-gray-600");
+  donateBtn.classList.remove("border-2", "border-solid", "text-gray-600");
 
-  historyBtn.classList.add("border-2", "border-solid", "p-[10px_30px_10px_30px]", "text-gray-600");
+  historyBtn.classList.add("border-2", "border-solid",  "text-gray-600");
 
-  historyBtn.classList.remove("bg-primary", "p-[12px_32px_12px_32px]", "text-black");
+  historyBtn.classList.remove("bg-primary",  "text-black");
 
   donateAllEl.classList.remove("hidden");
 
@@ -76,13 +76,13 @@ donateBtn.addEventListener("click", function () {
 });
 
 historyBtn.addEventListener("click", function () {
-  donateBtn.classList.remove("bg-primary", "p-[12px_32px_12px_32px]", "text-black");
+  donateBtn.classList.remove("bg-primary", "text-black");
 
-  donateBtn.classList.add("border-2", "border-solid", "border-dark-03", "p-[10px_30px_10px_30px]", "text-gray-600");
+  donateBtn.classList.add("border-2", "border-solid", "border-dark-03",  "text-gray-600");
 
-  historyBtn.classList.remove("border-2", "border-solid", "border-dark-03", "p-[10px_30px_10px_30px]", "text-gray-600");
+  historyBtn.classList.remove("border-2", "border-solid", "border-dark-03",  "text-gray-600");
 
-  historyBtn.classList.add("bg-primary", "p-[12px_32px_12px_32px]", "text-black");
+  historyBtn.classList.add("bg-primary",  "text-black");
 
   donateAllEl.classList.add("hidden");
 
